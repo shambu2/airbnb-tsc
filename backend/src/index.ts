cdee910b26ddd508d436454ed0c8e7e44b282aa1
shambu2 <mugalishambu601@gmail.com>
2025-05-14 08:08:34 +0530

    added models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express,{Request,Response} from "express";
-const app = express();
 import userRoutes from "./routes/user-routes"
-app.get('/',(req:Request,res:Response)=>{
-    res.send("hello from tsc")
-})
+// import { User } from "./models/user-model";
+
+
+const app = express();
+
 app.use('/api/v1/',userRoutes)
+app.use(express.json())
+
 
 app.listen(5000,()=>{
     console.log("server started on 5000 port")
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import { User } from '../models/user-model';
+
 const router = express.Router();
 
 router.get('/',(req,res)=>{
+    
     res.send("hello this is mvc")
 })
+router.post('/register',async(req,res)=>{
+    const {name,email,password} = req.body;
+    // try {
+    //     const saveUser = await User.create({
+    //         name,
+    //         email,
+    //         password
+    //     })
+    //     saveUser.save();
+    // } catch (error) {
+    //     console.log("error")
+    //     res.send("error occured")
+    // }
+    console.log(name)
+})
 
-// export default router;
 export default router;
